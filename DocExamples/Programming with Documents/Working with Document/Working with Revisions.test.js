b140f5be8be39b453bf3e04d6f947d80e3728ce0
Alexey Butalov <alexey.butalov@aspose.com>
2025-08-06 11:20:48 +0500

    Aspose.Words for Node.js via .Net 25.8 release

---
+++ DocExamples/Programming with Documents/Working with Document/Working with Revisions.test.js
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2001-2025 Aspose Pty Ltd. All Rights Reserved.
+//
+// This file is part of Aspose.Words. The source code in this file
+// is only intended as a supplement to the documentation, and is provided
+// "as is", without warranty of any kind, either expressed or implied.
+//////////////////////////////////////////////////////////////////////////
+
+const aw = require('@aspose/words');
+const base = require('../../DocExampleBase').DocExampleBase;
+const fs = require('fs');
+const path = require('path');
+
+
+describe("WorkingWithRevisions", () => {
+  beforeAll(() => {
+    base.oneTimeSetup();
+  });
+
+  afterAll(() => {
+    base.oneTimeTearDown();
+  });
+
+  
+  test('ShowRevisionsInBalloons', () => {
+    //ExStart:ShowRevisionsInBalloons
+    //GistId:829442fe4196eb8eb1ec945902f2f8ae
+    let doc = new aw.Document(base.myDir + "Revisions.docx");
+
+    doc.layoutOptions.revisionOptions.showInBalloons = aw.Layout.ShowInBalloons.FormatAndDelete;
+    doc.layoutOptions.revisionOptions.measurementUnit = aw.MeasurementUnits.Inches;
+    // Renders revision bars on the right side of a page.
+    doc.layoutOptions.revisionOptions.revisionBarsPosition = aw.Drawing.HorizontalAlignment.Right;
+
+    doc.save(base.artifactsDir + "WorkingWithRevisions.ShowRevisionsInBalloons.pdf");
+    //ExEnd:ShowRevisionsInBalloons
+  });
+
+});
---
+++ DocExamples/Rendering and Printing/Rendering shapes.test.js
@@ -0,0 +1,67 @@
+﻿// Copyright (c) 2001-2025 Aspose Pty Ltd. All Rights Reserved.
+//
+// This file is part of Aspose.Words. The source code in this file
+// is only intended as a supplement to the documentation, and is provided
+// "as is", without warranty of any kind, either expressed or implied.
+//////////////////////////////////////////////////////////////////////////
+
+const aw = require('@aspose/words');
+const base = require('../DocExampleBase').DocExampleBase;
+const fs = require('fs');
+const path = require('path');
+
+
+describe("RenderingShapes", () => {
+  beforeAll(() => {
+    base.oneTimeSetup();
+  });
+
+  afterAll(() => {
+    base.oneTimeTearDown();
+  });
+
+  
+  test('RenderShapeAsEmf', () => {
+    let doc = new aw.Document(base.myDir + "Rendering.docx");
+    let shape = doc.getShape(0, true);
+
+    //ExStart:RenderShapeAsEmf
+    //GistId:e9a02a29ae68be63f1fdfa266a642ea1
+    let renderer = shape.getShapeRenderer();
+    let saveOptions = new aw.Saving.ImageSaveOptions(aw.SaveFormat.Emf)
+    {
+        Scale = 1.5
+    };
+    renderer.save(base.artifactsDir + "RenderShape.RenderShapeAsEmf.emf", saveOptions);
+    //ExEnd:RenderShapeAsEmf
+  });
+
+
+  test('RenderShapeAsJpeg', () => {
+    let doc = new aw.Document(base.myDir + "Rendering.docx");
+    let shape = doc.getShape(0, true);
+
+    //ExStart:RenderShapeAsJpeg
+    //GistId:e9a02a29ae68be63f1fdfa266a642ea1
+    let renderer = shape.getShapeRenderer();
+    let saveOptions = new aw.Saving.ImageSaveOptions(aw.SaveFormat.Jpeg)
+    {
+        ImageColorMode = aw.Saving.ImageColorMode.Grayscale
+        ImageBrightness = 0.45
+
+    };
+    renderer.save(base.artifactsDir + "RenderShape.RenderShapeAsJpeg.jpg", saveOptions);
+    //ExEnd:RenderShapeAsJpeg
+  });
+
+  test('RenderShapeImage', () => {
+    let doc = new aw.Document(base.myDir + "Rendering.docx");
+    let shape = doc.getShape(0, true);
+
+    //ExStart:RenderShapeImage
+    //GistId:e9a02a29ae68be63f1fdfa266a642ea1
+    shape.getShapeRenderer().save(base.artifactsDir + "RenderShape.RenderShapeImage.jpg", new aw.Saving.ImageSaveOptions(aw.SaveFormat.Jpeg));
+    //ExEnd:RenderShapeImage
+  });
+
+});
